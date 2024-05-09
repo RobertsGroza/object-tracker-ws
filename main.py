f1d82c55ae4b7b41024b873b46b2d8867062dd9d
RobertsGroza <roberts.groza23@gmail.com>
2024-05-10 01:48:16 +0300

    Add ability to change the video by loading frames in chunks

--- main.py
+++ main.py
@@ -12,71 +12,97 @@ print("Server listening on Port " + str(PORT))
 
 # A set of connected ws clients
 connected = set()
-videos = os.listdir("../object-tracker-shared/videos")
-video = "riteni"
+videos = list(map(lambda el: el.split(".")[0], os.listdir("../object-tracker-shared/videos")))
+
+
+class VideoReader:
+    isBuffering = False
+
+    def __init__(self, websocket):
+        self.websocket = websocket
+        self.isBuffering = False
+        self.stream_width = 640
+        self.stream_height = 360
+        self.cap = None
+        self.position_file = None
+
+    def start(self, video_name):
+        self.isBuffering = True
+        self.cap = cv2.VideoCapture(f'../object-tracker-shared/videos/{video_name}.mp4')
+        self.position_file = open(f'../object-tracker-shared/outputs/{video_name}.txt', "r")
+
+    def stop(self):
+        self.isBuffering = False
+
+    async def get_first_frame(self):
+        test = self.position_file.readline().strip()
+        video_summary = json.loads(test)
+        await self.websocket.send(json.dumps({"type": "video_summary", "content": video_summary}))
+
+    async def get_next_frame(self):
+        success, img = self.cap.read()
+
+        if not success:
+            await self.dispose()
+            return
+
+        # Read frame information
+        object_positions = json.loads(self.position_file.readline().strip())
+
+        # Get coefficients for object position & width adjustments to stream size
+        width_scale_coefficient = self.stream_width / img.shape[1]
+        height_scale_coefficient = self.stream_height / img.shape[0]
+
+        # Process frame
+        resized = cv2.resize(img, (self.stream_width, self.stream_height))
+        success, buffer = cv2.imencode('.jpg', resized, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
+
+        # Process & adjust object information
+        for object_position in object_positions:
+            object_position["x"] = object_position["x"] * width_scale_coefficient
+            object_position["y"] = object_position["y"] * height_scale_coefficient
+            object_position["width"] = object_position["width"] * width_scale_coefficient
+            object_position["height"] = object_position["height"] * height_scale_coefficient
+
+        await self.websocket.send(json.dumps({
+            "type": "video_frame",
+            "frame": base64.b64encode(buffer).decode("utf-8"),
+            "positions": object_positions
+        }))
+
+    async def dispose(self):
+        self.isBuffering = False
+        await self.websocket.send(json.dumps({"type": "video_end"}))
+        self.position_file.close()
+        self.cap.release()
+
 
 async def echo(websocket):
     # Store a copy of the connected client
     print("A client just connected")
     connected.add(websocket)
+    await websocket.send(json.dumps({"type": "video_list", "videos": videos}))
+    reader = VideoReader(websocket)
 
     # Handle incoming messages
     try:
         async for message in websocket:
             print("Received message from client: " + message)
+            parsed_message = json.loads(message)
 
-            if message == "get_videos":
-                await websocket.send(json.dumps({"type": "video_list", "videos": videos}))
+            if parsed_message["type"] == "stop_buffer":
+                reader.stop()
 
-            if message == "play":
-                cap = cv2.VideoCapture(f'../object-tracker-shared/videos/{video}.mp4')
-                position_file = open(f'../object-tracker-shared/outputs/{video}.txt')
+            elif parsed_message["type"] == "play":
+                reader.start(parsed_message["content"])
+                await reader.get_first_frame()
 
-                prev_frame_time = 0
-                frame_count = 0
-                fps_sum = 0
-                stream_width = 640
-                stream_height = 360
-                success, img = cap.read()
+            elif parsed_message["type"] == "get_frames":
+                for i in range(int(parsed_message["content"])):
+                    await reader.get_next_frame()
 
-                video_summary = json.loads(position_file.readline().strip())
-                await websocket.send(json.dumps({"type": "video_summary", "content": video_summary}))
-
-                while success:
-                    new_frame_time = time.time()
-                    frame_count += 1
-
-                    # Get coefficients for object position & width adjustments to stream size
-                    width_scale_coefficient = stream_width / img.shape[1]
-                    height_scale_coefficient = stream_height / img.shape[0]
-
-                    # Process frame
-                    resized = cv2.resize(img, (stream_width, stream_height))
-                    success, buffer = cv2.imencode('.jpg', resized, [int(cv2.IMWRITE_JPEG_QUALITY), 30])
-
-                    # Process & adjust object information
-                    object_positions = json.loads(position_file.readline().strip())
-                    for object_position in object_positions:
-                        object_position["x"] = object_position["x"] * width_scale_coefficient
-                        object_position["y"] = object_position["y"] * height_scale_coefficient
-                        object_position["width"] = object_position["width"] * width_scale_coefficient
-                        object_position["height"] = object_position["height"] * height_scale_coefficient
-
-                    # Send message
-                    await websocket.send(json.dumps({
-                        "type": "video_frame",
-                        "frame": base64.b64encode(buffer).decode("utf-8"),
-                        "positions": object_positions
-                    }))
-
-                    success, img = cap.read()
-                    fps = 1 / (new_frame_time - prev_frame_time)
-                    prev_frame_time = new_frame_time
-                    fps_sum += fps
-                await websocket.send(json.dumps({"type": "video_end"}))
-                print("AVERAGE FPS: " + str(fps_sum / frame_count))
             else:
-                await websocket.send("Unknown message!")
+                await websocket.send(json.dumps({"type": "error", "description": "Unknown message!"}))
 
     # Handle disconnecting clients
     finally:
